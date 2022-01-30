b9ef19a4b3a6235464c5df302ecbc4b83e673773
Tan Nguyen <tannguyen1220@gmail.com>
2022-01-30 16:39:01 +0700

    read some content

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from bs4 import BeautifulSoup
+
+# doc file html local
+with open('index.html', 'r') as htmlFile:
+    content = htmlFile.read()
+    # read the content with beauty ful soup
+    soup = BeautifulSoup(content,'lxml')
+
+    tags = soup.find_all('div',class_='price')
+    for tag in tags:
+        print(tag.span)
