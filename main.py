65c25a41457c7f5c9857b2d8d0922ade8be7405e
Tan Nguyen <tannguyen1220@gmail.com>
2022-02-01 18:26:09 +0700

    show more info

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ soup = BeautifulSoup(htmlContent,'lxml')
 newCards = soup.find_all('div',class_='information svelte-kncn9e')
 
 for newCard in newCards:
-    tile = newCard.find('p', class_='title svelte-kncn9e')
-    print (tile)
+    tile = newCard.find('p', class_='title svelte-kncn9e').text
+    updateTime = newCard.find('div', class_='sub-text full-width svelte-kncn9e').text
+    print ("tile: "+ tile)
+    print ("info: "+ updateTime)
 
