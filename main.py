1f5e961d23c45644ea6977c12183bf771badb437
Tan Nguyen <tannguyen1220@gmail.com>
2022-02-01 18:43:09 +0700

    even more infomation

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ htmlContent = requests.get('https://www.masterduelmeta.com/').text
 
 soup = BeautifulSoup(htmlContent,'lxml')
 
-newCards = soup.find_all('div',class_='information svelte-kncn9e')
+newCards = soup.find_all('div',class_='perspective svelte-kncn9e')
 
 for newCard in newCards:
     tile = newCard.find('p', class_='title svelte-kncn9e').text
     updateTime = newCard.find('div', class_='sub-text full-width svelte-kncn9e').text
+    anchorLink = newCard.find('a',class_='image-card svelte-kncn9e')
+    anchorHref = anchorLink['href']
     print ("tile: "+ tile)
     print ("info: "+ updateTime)
+    print ("link:" + 'https://www.masterduelmeta.com'+anchorHref)
 
